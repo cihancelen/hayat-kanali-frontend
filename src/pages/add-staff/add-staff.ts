4a450ebf792555fecbd2d61d66083b27fd4357e7
Cihan Çelen <cihan.celen37@hotmail.com>
2019-02-04 00:29:40 +0300

    personel ekleme ekranı validation bitti.

--- src/pages/add-staff/add-staff.ts
+++ src/pages/add-staff/add-staff.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HospitalService } from '../../services/hospital.service';
 import { NotificationService } from '../../services/notification.service';
 import { HomePage } from '../home/home';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { patterns } from '../../classes/regex-pattern';
 
 @Component({
   selector: 'page-add-staff',
   templateUrl: 'add-staff.html',
 })
-export class AddStaffPage {
+export class AddStaffPage implements OnInit {
 
   constructor(
     private hospitalService: HospitalService,
     private notificationService: NotificationService,
-    private navCtrl: NavController 
+    private navCtrl: NavController,
+    private formBuilder: FormBuilder
   ) { }
 
   employee: any = {
@@ -25,14 +28,33 @@ export class AddStaffPage {
     hospitalId: JSON.parse(localStorage.getItem('hospital-info')).id
   };
 
-  addEmployee() {
-    this.hospitalService.addEmployee(this.employee).subscribe(data =>{
-      this.notificationService.notification('Personel başarıyla eklenmiştir.');
+  employeeForm: FormGroup;
+  isSubmit: boolean = false;
 
-      this.navCtrl.setRoot(HomePage);
-    },err =>{
-      console.log(err);
-    })
+  ngOnInit() {
+    this.employeeForm = this.formBuilder.group({
+      'name': ['', [Validators.required]],
+      'surname': ['', [Validators.required]],
+      'email': ['', [Validators.required, Validators.email, Validators.pattern(patterns.emailPattern)]],
+      'username': ['', [Validators.required]],
+      'password': ['', [Validators.required]]
+    });
+  }
+
+  get f() { return this.employeeForm.controls; }
+
+  addEmployee() {
+    this.isSubmit = true;
+
+    if (this.employeeForm.valid) {
+      this.hospitalService.addEmployee(this.employee).subscribe(data => {
+        this.notificationService.notification('Personel başarıyla eklenmiştir.');
+
+        this.navCtrl.setRoot(HomePage);
+      }, err => {
+        console.log(err);
+      })
+    }
   }
 
 }
