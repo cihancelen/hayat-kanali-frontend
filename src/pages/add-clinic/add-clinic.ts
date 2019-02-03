aa6022e2dc0e3015d0dc5ee0260d9a662eb89582
Cihan Çelen <cihan.celen37@hotmail.com>
2019-02-04 00:36:47 +0300

    klinik ekleme sayfası validation yapıldı.

--- src/pages/add-clinic/add-clinic.ts
+++ src/pages/add-clinic/add-clinic.ts
@@ -1,34 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HospitalService } from '../../services/hospital.service';
 import { NotificationService } from '../../services/notification.service';
 import { HomePage } from '../home/home';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-add-clinic',
   templateUrl: 'add-clinic.html',
 })
-export class AddClinicPage {
+export class AddClinicPage implements OnInit {
 
   constructor(
     private hospitalService: HospitalService,
     private notificationService: NotificationService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private formBuilder: FormBuilder
   ) { }
 
   clinic: any = {
-    name:'',
+    name: '',
     description: ''
   }
 
-  addClinic() {
-    this.hospitalService.addClinic(this.clinic).subscribe(data =>{
-      this.notificationService.notification('Klinik başarıyla eklenmiştir.');
+  clinicForm: FormGroup;
+  isSubmit: boolean = false;
 
-      this.navCtrl.setRoot(HomePage);
-    }, err =>{
-      console.log(err);
+  ngOnInit() {
+    this.clinicForm = this.formBuilder.group({
+      'name': ['', [Validators.required]],
+      'desc': ['', [Validators.required]]
     });
+  }
+
+  get f() { return this.clinicForm.controls; }
+
+  addClinic() {
+    this.isSubmit = true;
+
+    if (this.clinicForm.valid) {
+      this.hospitalService.addClinic(this.clinic).subscribe(data => {
+        this.notificationService.notification('Klinik başarıyla eklenmiştir.');
+
+        this.navCtrl.setRoot(HomePage);
+      }, err => {
+        console.log(err);
+      });
+    }
   }
 
 }
